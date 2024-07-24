26b41142ed80162865859801db27545320b5c4a3
outsidermm <71582657+outsidermm@users.noreply.github.com>
2024-07-24 18:10:04 +1000

    reformatted statistic bar

---
+++ gui/statistic_bar.py
@@ -0,0 +1,55 @@
+from pygame_gui.elements import UIStatusBar
+
+class StatisticBar(UIStatusBar):
+    """
+    Custom UIStatusBar class to represent a statistic bar with text.
+
+    Attributes:
+        __bar_text (str): Text to display on the bar.
+    """
+
+    def __init__(
+        self,
+        text: str = None,
+        *args,
+        **kwargs,
+    ) -> None:
+        """
+        Initializes the StatisticBar with a text and other arguments.
+
+        :param text: Text to display on the bar.
+        """
+        self.set_bar_text(text)
+        super().__init__(*args, **kwargs)
+
+    def status_text(self) -> str:
+        """
+        Returns the text of the bar.
+
+        :return: The bar text.
+        """
+        return self.get_bar_text()
+
+    def set_text(self, text: str) -> None:
+        """
+        Sets the text of the bar.
+
+        :param text: Text to set.
+        """
+        self.set_bar_text(text)
+
+    def get_bar_text(self) -> str:
+        """
+        Gets the current text of the bar.
+
+        :return: The bar text.
+        """
+        return self.__bar_text
+
+    def set_bar_text(self, text: str) -> None:
+        """
+        Sets the text of the bar.
+
+        :param text: Text to set.
+        """
+        self.__bar_text = text
--- gui/statistic_hud.py
+++ gui/statistic_hud.py
@@ -1,31 +1,19 @@
 import pygame
 import pygame_gui
-from pygame_gui.elements import UITextBox, UIPanel, UIStatusBar
+from pygame_gui.elements import UITextBox, UIPanel
 from pygame_gui.core import ObjectID
-
-
-class StatisticBar(UIStatusBar):
-    def __init__(
-        self,
-        text: str = None,
-        *args,
-        **kwargs,
-    ) -> None:
-        self.__bar_text = text
-        super().__init__(*args, **kwargs)
-
-    def status_text(self) -> str:
-        return self.__bar_text
-
-    def set_text(self, text: str) -> None:
-        self.__bar_text = text
-
+from gui.statistic_bar import StatisticBar
 
 class StatisticHUD:
-    __stat_text: UITextBox = None
-    __stat_bar: StatisticBar = None
-    __stats: dict[str, int] = None
-    __max_stat_value: int = None
+    """
+    HUD component to display character statistics using text and bars.
+
+    Attributes:
+        __stat_text (UITextBox): The text box displaying the stat name.
+        __stat_bar (StatisticBar): The bar representing the stat value.
+        __stats (dict[str, int]): Dictionary of character stats.
+        __max_stat_value (int): The maximum value for the stat.
+    """
 
     def __init__(
         self,
@@ -35,25 +23,37 @@ class StatisticHUD:
         stat_name: str,
         max_stat_value: int,
         stat_count: int,
+        init_text_x : int= 50,
+        init_bar_x : int= 300,
+        init_y : int= 108,
+        gap_per_stats: int = 50,
     ) -> None:
-        self.__stats = stats
-        self.__max_stat_value = max_stat_value
+        """
+        Initializes the StatisticHUD with the provided parameters.
 
-        init_text_x = 50
-        init_bar_x = 300
-        init_y = 108
-        gap_per_stats = 50
+        :param ui_manager: The UI manager to manage the HUD components.
+        :param container: The container panel for the HUD.
+        :param stats: Dictionary of character stats.
+        :param stat_name: The name of the stat to display.
+        :param max_stat_value: The maximum value of the stat for normalization.
+        :param stat_count: The index of the stat to position it correctly.
+        """
+        self.set_stats(stats)
+        self.set_max_stat_value(max_stat_value)
 
-        self.__stat_text = UITextBox(
-            html_text=f'<img src="assets/icons_48/{stat_name}.png"> '
-            f"{" ".join(word.capitalize() for word in stat_name.split("_"))}",
-            relative_rect=pygame.Rect(
-                (init_text_x, init_y + stat_count * gap_per_stats),
-                (300, -1),
-            ),
-            manager=ui_manager,
-            container=container,
-            object_id=ObjectID(object_id="#character_info"),
+        # Create the text box for the stat name
+        self.set_stat_text(
+            UITextBox(
+                html_text=f'<img src="assets/icons_48/{stat_name}.png"> '
+                f"{' '.join(word.capitalize() for word in stat_name.split('_'))}",
+                relative_rect=pygame.Rect(
+                    (init_text_x, init_y + stat_count * gap_per_stats),
+                    (300, -1),
+                ),
+                manager=ui_manager,
+                container=container,
+                object_id=ObjectID(object_id="#character_info"),
+            )
         )
 
         bar_location = pygame.Rect(
@@ -61,16 +61,19 @@ class StatisticHUD:
             (200, 30),
         )
         bar_location.right = -50
-        numerical_stat = stats[stat_name] if stat_name in stats.keys() else 0
+        numerical_stat = stats.get(stat_name, 0)
 
-        self.__stat_bar = StatisticBar(
-            relative_rect=bar_location,
-            manager=ui_manager,
-            anchors=({"right": "right"}),
-            percent_method=(lambda stat_name=stat_name,: self.progress_bar(stat_name)),
-            container=container,
-            object_id=ObjectID(class_id="@statistics_bar"),
-            text=f"{numerical_stat}/{max_stat_value}",
+        # Create the bar for the stat value
+        self.set_stat_bar(
+            StatisticBar(
+                relative_rect=bar_location,
+                manager=ui_manager,
+                anchors={"right": "right"},
+                percent_method=lambda stat_name=stat_name: self.progress_bar(stat_name),
+                container=container,
+                object_id=ObjectID(class_id="@statistics_bar"),
+                text=f"{numerical_stat}/{max_stat_value}",
+            )
         )
 
     def update(
@@ -79,20 +82,100 @@ class StatisticHUD:
         stat_name: str,
         max_stat_value: int,
     ) -> None:
-        numerical_stat = stats[stat_name] if stat_name in stats.keys() else 0
-        self.__stat_bar.set_text(f"{numerical_stat}/{max_stat_value}")
-        self.__stat_bar.redraw()
+        """
+        Updates the HUD to reflect new stat values.
 
-        self.__stats = stats
-        self.__max_stat_value = max_stat_value
+        :param stats: Dictionary of character stats.
+        :param stat_name: The name of the stat to update.
+        :param max_stat_value: The new maximum value of the stat for normalization.
+        """
+        numerical_stat = stats.get(stat_name, 0)
+        self.get_stat_bar().set_text(f"{numerical_stat}/{max_stat_value}")
+        self.get_stat_bar().redraw()
+
+        self.set_stats(stats)
+        self.set_max_stat_value(max_stat_value)
 
     def kill(self) -> None:
-        self.__stat_text.kill()
-        self.__stat_bar.kill()
+        """
+        Kills (removes) the HUD components.
+        """
+        self.get_stat_text().kill()
+        self.get_stat_bar().kill()
 
     def progress_bar(self, stat_name: str) -> float:
+        """
+        Computes the progress of the stat bar as a percentage of the maximum value.
+
+        :param stat_name: The name of the stat.
+        :return: The progress percentage of the stat bar.
+        """
         return (
-            self.__stats[stat_name] / self.__max_stat_value
-            if stat_name in self.__stats.keys()
+            self.get_stats().get(stat_name, 0) / self.get_max_stat_value()
+            if self.get_max_stat_value() > 0
             else 0
         )
+
+    def get_stat_text(self) -> UITextBox:
+        """
+        Gets the text box for the stat.
+
+        :return: The stat text box.
+        """
+        return self.__stat_text
+
+    def set_stat_text(self, stat_text: UITextBox) -> None:
+        """
+        Sets the text box for the stat.
+
+        :param stat_text: The stat text box to set.
+        """
+        self.__stat_text = stat_text
+
+    def get_stat_bar(self) -> StatisticBar:
+        """
+        Gets the bar for the stat.
+
+        :return: The stat bar.
+        """
+        return self.__stat_bar
+
+    def set_stat_bar(self, stat_bar: StatisticBar) -> None:
+        """
+        Sets the bar for the stat.
+
+        :param stat_bar: The stat bar to set.
+        """
+        self.__stat_bar = stat_bar
+
+    def get_stats(self) -> dict[str, int]:
+        """
+        Gets the dictionary of character stats.
+
+        :return: The dictionary of character stats.
+        """
+        return self.__stats
+
+    def set_stats(self, stats: dict[str, int]) -> None:
+        """
+        Sets the dictionary of character stats.
+
+        :param stats: The dictionary of character stats to set.
+        """
+        self.__stats = stats
+
+    def get_max_stat_value(self) -> int:
+        """
+        Gets the maximum value of the stat for normalization.
+
+        :return: The maximum stat value.
+        """
+        return self.__max_stat_value
+
+    def set_max_stat_value(self, max_stat_value: int) -> None:
+        """
+        Sets the maximum value of the stat for normalization.
+
+        :param max_stat_value: The maximum stat value to set.
+        """
+        self.__max_stat_value = max_stat_value
